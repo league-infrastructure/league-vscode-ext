05869a8b0a0c53788b1e7dc8968907bab42b431e
Eric Busboom <eric@civicknowledge.com>
2025-01-22 18:06:19 -0800

    Bump version to 0.2.1, add new monitor icons for light and dark themes, and update command menu structure

--- src/virtdisplay.ts
+++ src/virtdisplay.ts
@@ -3,24 +3,18 @@ import * as vscode from 'vscode';
 
 export function activateVirtDisplay(context: vscode.ExtensionContext) {
 
-    console.log('Installing the virtual display to url: ' + process.env.VNC_URL);
 
     context.subscriptions.push(
         vscode.commands.registerCommand('jointheleague.openVirtualDisplay', async () => {
-            // Get the VNC_URL from environment variables
+ 
             const vncUrl = process.env.VNC_URL;
 
-            // Check if VNC_URL is defined
             if (!vncUrl) {
                 vscode.window.showErrorMessage('VNC_URL environment variable is not set');
                 return;
             }
 
-            vscode.window.showInformationMessage('Open virtual display: ' + process.env.VNC_URL);
-
             try {
-                // Execute sequence of commands
-                //await vscode.commands.executeCommand('workbench.action.splitEditorRight');
                 await vscode.commands.executeCommand('simpleBrowser.api.open', vncUrl);
                 await vscode.commands.executeCommand('workbench.action.moveEditorToRightGroup');
             } catch (error) {
@@ -30,5 +24,4 @@ export function activateVirtDisplay(context: vscode.ExtensionContext) {
     );
 }
 
-// Optional: Deactivate function if you need cleanup
 export function deactivate() {}
